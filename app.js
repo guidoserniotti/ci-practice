39c048a1b8be5663bbf18ec4b481c43f982a9dc2
guidoserniotti <guido.serniotti.13@gmail.com>
2026-03-20 19:04:23 -0300

    testing PR fails

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Received a request.');
+    console.log('THIS IS GOING TO FAIL THE PR :)');
     const msg = 'Works on my machine.';
     res.send(msg);
 });
