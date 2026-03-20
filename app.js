4e23c9ce816b190d10fd1497ba7e93fd6910bddc
guidoserniotti <guido.serniotti.13@gmail.com>
2026-03-20 17:43:55 -0300

    purposely added lint error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log('Received a request to the root route');
     const msg = 'Hello World!';
     res.send(msg);
 });
