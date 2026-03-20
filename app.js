e2964e54cff257fbb4ce5591373890c1426f7b78
guidoserniotti <guido.serniotti.13@gmail.com>
2026-03-20 17:41:32 -0300

    eslint init

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Works on my machine.');
+    const msg = 'Hello World!';
+    res.send(msg);
 });
 
 module.exports = app;
