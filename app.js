8d068092ae079e21927141768d88d2e188fc2f23
guidoserniotti <guido.serniotti.13@gmail.com>
2026-03-20 17:10:52 -0300

    .prettierrc

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello.");
+app.get('/', (req, res) => {
+    res
+    .
+    send
+    ('Hello.');
 });
 
 module.exports = app;
